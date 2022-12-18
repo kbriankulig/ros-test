9cb47a80854c39236068adf5c285322c6e5e6765
kbriankulig <kbriankulig@gmail.com>
2022-12-17 20:19:27 -0500

    Added check of Ubuntu version from python

--- python_test_script.py
+++ python_test_script.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 
+import os
+
 def callback():
     print("Hello from python_test_script.py")
+    
+    print("Checking Ubuntu version")
+    os.system("lsb_release -a")
 
+    print("Done with python script")
+    
 if __name__ == '__main__':
     callback()
