a7f2f0e3f67f8a3f208d7acba484c1ecdd403b18
kbriankulig <kbriankulig@gmail.com>
2022-12-20 13:13:51 -0500

    Added rostests

--- test_ws/src/test_pkg/test/overall_test_status.py
+++ test_ws/src/test_pkg/test/overall_test_status.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python3
 
-# This script is meant to be called from a docker running on github Actions.
-# Check to see if there were any errors reported by catkin_test_results.
-#  If there were 0 errors return a succesful system exit code of 0 for git Actions. Otherwise return 1.
+# This script is meant to be called from a docker running using github Actions.
+# This script runs catkin_test_results on the output to decide if the corresponding github Action should be failed.
+# If catkin_test_results contain the string "0 errors", return a succesful system exit code of 0 for git Actions,
+# otherwise return 1.to fail the git Action.
         
 if __name__ == '__main__':
     import subprocess
     import sys
     
-    # Note that "/root" is "/github/home" at the end of the system command.  Don't really understand this - kbk...
-    system_command = 'source /opt/ros/noetic/setup.bash; source /root/test_ws/devel/setup.bash; catkin_test_results /github/home/.ros/test_results'
+    # Build a shell command to send to a bash shell subprocess
+    system_command = 'source /opt/ros/noetic/setup.bash; '
+    system_command += 'source /root/test_ws/devel/setup.bash; '
+    system_command += 'catkin_test_results /github/home/.ros/test_results'
     print(system_command)
     
+    
+    # Run shell command
     result = subprocess.run([system_command], stdout=subprocess.PIPE, shell=True, executable='/bin/bash')
     print(result.stdout.decode("utf-8"))
     
     # Check if catkin_test_results contains the string "0 errors"
     if "0 errors" in result.stdout.decode("utf-8"):
-        print("No errors in catkin_test_results.")
+        print("No errors reported with catkin_test_results.")
         sys.exit(0)
     else:
-        print("1 or more errors in catkin_test_results.")
+        print("1 or more errors reported with catkin_test_results.")
         sys.exit(1)
     
     
